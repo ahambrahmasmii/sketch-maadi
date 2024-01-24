eefc48416a917b082c4d4aea662b2bf4ac409c41
ahambrahmasmii <88248169+ahambrahmasmii@users.noreply.github.com>
2024-01-24 17:21:16 +0530

    Add files via upload

---
+++ components/Board/index.js
@@ -0,0 +1,137 @@
+import { MENU_ITEMS } from "@/constants";
+import { actionItemCLick } from "@/slice/menuSlice";
+import { socket } from "@/socket";
+import { useLayoutEffect,useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+const Board=()=>{
+    const dispatch=useDispatch()
+    const canvasRef=useRef(null)
+    const drawHistory=useRef([])
+    const historyPointer=useRef(0)
+    const shouldDraw=useRef(false)
+    const {activeMenuItem,actionMenuItem}=useSelector((state)=>state.menu)
+    const {color,size}= useSelector((state)=>state.toolbox[activeMenuItem])
+    
+    useEffect(()=>{
+        if(!canvasRef.current) return 
+        const canvas=canvasRef.current;
+        const context=canvas.getContext('2d')
+
+        if(actionMenuItem===MENU_ITEMS.DOWNLOAD){
+            const URL=canvas.toDataURL()
+            const anchor=document.createElement('a')
+            anchor.href=URL
+            anchor.download='sketch.jpg'
+            anchor.click()
+            //console.log(URL)
+
+        } else if(actionMenuItem===MENU_ITEMS.UNDO || actionMenuItem===MENU_ITEMS.REDO){
+            if(historyPointer.current>0 && actionMenuItem===MENU_ITEMS.UNDO) historyPointer.current-=1
+            if(historyPointer.current<drawHistory.current.length-1 && actionMenuItem===MENU_ITEMS.REDO) historyPointer.current+=1
+            const imageData=drawHistory.current[historyPointer.current]
+            context.putImageData(imageData,0,0)
+            
+        }
+        dispatch(actionItemCLick(null))
+        //console.log("actionMenuItem",actionMenuItem)
+
+    },[actionMenuItem, dispatch])
+
+
+    useEffect(()=>{
+
+        if(!canvasRef.current) return 
+        const canvas=canvasRef.current;
+        const context=canvas.getContext('2d')
+        
+        const changeConfig=(color,size)=>{
+            context.strokeStyle=color
+        context.lineWidth=size
+
+        }
+        
+       const handleChangeConfig=(config)=>{
+        changeConfig(config.color,config.size)
+       }
+        changeConfig(color,size)
+        socket.on('changeConfig', handleChangeConfig)
+        
+        return()=>{
+            socket.off('changeConfig',handleChangeConfig)
+        }
+
+},[color,size])
+
+
+    useLayoutEffect(()=>{
+        if(!canvasRef.current) return 
+        const canvas=canvasRef.current;
+        const context=canvas.getContext('2d')
+
+        //when mounting
+
+
+        canvas.width=window.innerWidth
+        canvas.height=window.innerHeight
+        
+        const beginPath=(x,y)=>{
+            context.beginPath()
+            context.moveTo(x,y)
+        }
+
+        const drawLine=(x,y)=>{
+            context.lineTo(x,y)
+            context.stroke()
+        }
+        const handleMouseDown=(e)=>{
+            shouldDraw.current=true
+            beginPath(e.clientX,e.clientY)
+            //context.moveTo()
+            socket.emit('beginPath',{x:e.clientX,y:e.clientY})
+        }
+        const handleMouseMove=(e)=>{
+            if(!shouldDraw.current) return
+            drawLine(e.clientX,e.clientY)
+            //context.stroke()
+            socket.emit('drawLine',{x:e.clientX,y:e.clientY})
+        }
+        const handleMouseUp=(e)=>{
+            shouldDraw.current=false
+            const imageData=context.getImageData(0,0,canvas.width,canvas.height)
+            drawHistory.current.push(imageData)
+            historyPointer.current=drawHistory.current.length-1
+        }
+
+        const handleBeginPath=(path)=>{
+            beginPath(path.x,path.y)
+        }
+
+        const handleDrawLine=(path)=>{
+            drawLine(path.x,path.y)
+        }
+
+        canvas.addEventListener('mousedown',handleMouseDown)
+        canvas.addEventListener('mousemove',handleMouseMove)
+        canvas.addEventListener('mouseup', handleMouseUp)
+
+        /*socket.on("connect",()=>{
+            console.log('client connected');        
+        })*/
+
+        socket.on('beginPath',handleBeginPath)
+        socket.on('drawLine',handleDrawLine)
+
+        return () =>{
+            canvas.removeEventListener('mousedown',handleMouseDown)
+        canvas.removeEventListener('mousemove',handleMouseMove)
+        canvas.removeEventListener('mouseup', handleMouseUp)
+
+        
+
+        }
+
+    },[])
+    return(<canvas ref={canvasRef}></canvas>)
+}
+
+export default Board;
---
+++ slice/menuSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { MENU_ITEMS } from "@/constants";
+const initialState={
+    activeMenuItem:MENU_ITEMS.PENCIL,
+    actionMenuItem:null
+}
+export const menuSlice = createSlice({
+    name:"menu",
+    initialState,
+    reducers:{
+        menuItemClick:(state,action)=>{
+            state.activeMenuItem=action.payload
+        },
+        actionItemCLick:(state,action)=>{
+            state.actionMenuItem=action.payload
+        }
+    }
+})
+
+export const {menuItemClick,actionItemCLick}=menuSlice.actions
+export default menuSlice.reducer
